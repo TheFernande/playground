855abfa3fba3fc2445b7dc2f9af9862356a2d344
Jade Hernandez <jade.hernandez88@gmail.com>
2024-12-11 15:34:12 +0100

    Setup for a new NextJS project with TailwindCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="main">
-      <h1>Home</h1>
+    <main className="flex flex-col items-center justify-center p-4 mx-auto min-h-screen">
+      <h1 className="text-4xl font-semibold">Home</h1>
     </main>
   );
 }
