550edcf6ae7ffa11047167a0db5cbe7c644ff869
Jade Hernandez <jade.hernandez88@gmail.com>
2024-12-12 16:59:45 +0100

    ProfileCard component with social media icons and update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import ProfileCard from "../components/ProfileCard";
+
+import profilePic from "../../public/images/profile-thumbnail.png";
+
+const cardData = {
+  imageUrl: profilePic.src,
+  name: "Sarah Dole",
+  role: "Front End Engineer @ Microsoft",
+  bio: "I turn coffee into bugs which are fixed by someone else. Certified Stack Overflow and ChatGPT developer.",
+};
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center p-4 mx-auto min-h-screen">
-      <h1 className="text-4xl font-semibold">Home</h1>
+    <main className="mx-auto min-h-screen pt-[200px] bg-gradient-to-b from-[#F9FAFB] to-[#D2D6DB]">
+      <ProfileCard
+        imageUrl={cardData.imageUrl}
+        name={cardData.name}
+        role={cardData.role}
+        bio={cardData.bio}
+      />
     </main>
   );
 }
